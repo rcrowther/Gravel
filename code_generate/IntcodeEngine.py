4dbb732e1759bea52ad7b7add42c7d338698cfb3
Rob Crowther <rw.crowther@gmail.com>
2019-12-25 15:43:52 +0000

    progress

--- code_generate/IntcodeEngine.py
+++ code_generate/IntcodeEngine.py
@@ -122,53 +122,68 @@ class LocalAlloc():
 # StartStops
 #            
 class StartStop():
-    def __init__(self, idx, isStart, callCount):
+    def __init__(self, idx, isStart, prevCallIdx):
         self.idx = idx
         self.isStart = isStart
-        self.callCount = callCount
+        self.prevCallIdx = prevCallIdx
+        self.scopedCalls = None
 
     def __repr__(self):
-        return "StartStop(idx:{}, isStart:{}, callCount:{})".format(
-            self.idx, self.isStart, self.callCount
+        return "StartStop(idx:{}, isStart:{}, prevCallIdx:{}, scopedCalls:{})".format(
+            self.idx, self.isStart, self.prevCallIdx, self.scopedCalls
             ) 
                         
-def startStopStart(idx):
-    return StartStop(idx, True, 0)
+def startStopStart(idx, prevCallIdx):
+    return StartStop(idx, True, prevCallIdx)
 
-def startStopUse(idx, callCount):
-    return StartStop(idx, False, callCount)
+def startStopUse(idx, prevCallIdx):
+    return StartStop(idx, False, prevCallIdx)
 
 
 class StartStopBuilder():
     # Filter data usage to first start and stop
-    # - Parameters are treated as a local variable.
-    # - All variables, uncluding parameters, must be given a unique and
-    # serial id.
-    # - Parameters must be declared with the correct action.
+    # The indexes given for labels are unique and serial ids.
+    # Parameters are treated as a local variable. They should not be 
+    # noted at source (i.e. at top of call code) but when they are first
+    # used (even if not an allocation).
+    # However, if a label is the same as a param, call paramAsoc() also, 
+    # as the data is needed.
+    # Call() should be used whenever there is a call. It is treated as 
+    # an index.
     def __init__(self):
         self.startStops = []
         self.maxIdx = 0
-         
+        self.callCount = -1
+        self.paramAssocs = {}
+        
+    def call(self):
+        self.callCount += 1
+        
+    def paramAssoc(self, paramId, labelId):
+        self.paramAssocs[paramId] = labelId
+        
     def firstUse(self, idx):
-        self.startStops.append(startStopStart(idx))
+        self.startStops.append(startStopStart(idx, self.callCount))
         if (idx > self.maxIdx):
             self.maxIdx = idx
                 
-    def use(self, idx, callCount):
-        self.startStops.append(startStopUse(idx, callCount))
+    def use(self, idx):
+        self.startStops.append(startStopUse(idx, self.callCount))
 
     def result(self):
         b = []
-        # Also stashes callcount to transferr to the start marker.
-        usageEcountered = [-1 for a in range(0, (self.maxIdx + 1))]
+        # Also stashes callcount to transfer to the start marker.
+        usageEcountered = [False for a in range(0, (self.maxIdx + 1))]
+        tailPrevCallIdx = [-1 for a in range(0, (self.maxIdx + 1))]
         for ss in reversed(self.startStops):
             if (ss.isStart):
                 # copy callCount to start mark
-                ss.callCount = usageEcountered[ss.idx]
+                ss.scopedCalls = range(ss.prevCallIdx + 1, tailPrevCallIdx[ss.idx] + 1)
                 b.append(ss)
-            if ((not ss.isStart) and (usageEcountered[ss.idx] == -1)):
+            if ((not ss.isStart) and (usageEcountered[ss.idx] == False)):
                 b.append(ss)
-                usageEcountered[ss.idx] = ss.callCount
+                usageEcountered[ss.idx] = True
+                tailPrevCallIdx[ss.idx] = ss.prevCallIdx
         b.reverse()
         return b
 
@@ -178,7 +193,7 @@ class StartStopBuilder():
             )       
             
 #NB: a sketch, keep!
-# def stopStartScan(lines):
+# def stopStartScan(paramLabels, lines):
     # # rScan lines and return a start stop list
     # # The list is the first and last occurence of a label. It has
     # # no location, but is in order of appearence.
@@ -196,6 +211,9 @@ class StartStopBuilder():
     # while line in lines:
         # if line contains label:
             # if (labelActive[labelIdx] == -1):
+                # r = paramLabels.find(label)
+                # if (r != -1):
+                    # b.paramAssocs(r, labelIdx)
                 # b.firstUse(labelIdx)
                 # labelActive[labelIdx] = 0
                 # currentHighIdx = labelIdx
@@ -205,7 +223,12 @@ class StartStopBuilder():
            # for i in range(0, currentHighIdx):
                # labelActive[i] += 1 
     # return b.result()
+ 
     
+###
+# Allocing
+#
+
 def parameterAlloc(paramData, localAllocs, startStopB):
             
     if (action.isLeaf or action.callCount < 3):
@@ -260,6 +283,225 @@ def parameterAlloc(paramData, localAllocs, startStopB):
 #! Use parameter free list if no actions intrude.
 #! - these would have high priority
 #! lower param reg usage protection by only using in-scope protection
+
+MemLoc = collections.namedtuple("MemLoc", "isReg loc")
+def regMemLoc(loc):
+    return MemLoc(True, loc)
+
+def stackMemLoc(loc):
+    return MemLoc(False, loc)
+    
+class LiveAllocate():
+    # registers are from 0. Registers include param and nonparam
+    # in the same sequence.
+    # label ids are from 0. Labels do not include the initial statement
+    # of a parameter, but do include the subsequent use of parameters.
+    def __init__(self, 
+        paramCount,
+        paramAssocs, 
+        paramRegCount, 
+        nonParamRegCount, 
+        startStops,
+        labelCount,
+        ):
+        self.paramCount = paramCount
+        self.paramAssocs = paramAssocs
+        self.paramRegCount = paramRegCount
+        self.startStops = startStops
+        self.labelCount = labelCount
+        # Parameter registers are of limited use.
+        # At first they are locked and unavailable, so this is empty.
+        # On release, (by value allocation or out of scope) they
+        # are still of limited use, as they cannot cross a call
+        # (even a push/pop may not protect against subcalls).
+        # but they are a valuable resource for quick calculation
+        # witness the use of rax etc.
+        self.paramRegisters = [] #[r for r in range(0, paramRegCount)]
+        # All used non-param registers should be restored by 
+        # initial/funal push/pops (often convention) 
+        self.nonParamRegistersToProtect = set()
+        self.nonParamRegisters = [r for r in range(paramRegCount, paramRegCount + nonParamRegCount)]
+        # Array(Tuple(MemLoc(from), to))
+        self.initialParamTransfer = []
+        # if a paramreg wishes to sustain over a call, it must be 
+        # push/popped.
+        # Hash(callId:Array(paramReg))
+        self.callParamRegProtection = {}
+        # leap to start info easily
+        self.startAccess = [-1 for id in range(0, labelCount)]
+        for ss in self.startStops:
+            if ss.isStart:
+                self.startAccess[ss.idx] = ss
+        self.stackOffset = 8
+        self.stackFreeList = []
+        
+        # Sometimes a loc must be initialized from an internal shuffle.
+        # Two situations are a shunt from paramReg, and
+        # a shunt into a scope from stack.
+        # ArrayById(Tuple(from: MemLoc, to: MemLoc))
+        self.locInitialize = []
+
+        # finally, this is where a local is to be found
+        # Array(MemLoc) where array idx = localIdx
+        self.localAllocs = [-1 for id in range(0, labelCount)]
+        
+    def paramRegisterCanAlloc(self):
+        return (len(self.paramRegisters) > 0)
+
+    def paramRegisterAlloc(self):
+        rid = self.paramRegisters.pop()
+        return rid
+        
+    def paramRegisterDealloc(self, rid):
+        self.paramRegisters.append(rid)
+                
+    def nonParamRegisterCanAlloc(self):
+        return (len(self.nonParamRegisters) > 0)
+
+    def nonParamRegisterAlloc(self):
+        rid = self.nonParamRegisters.pop()
+        self.nonParamRegistersToProtect.add(rid)
+        return rid
+
+    def nonParamRegisterDealloc(self, rid):
+        self.nonParamRegisters.append(rid)
+        
+    def stackAlloc(self):
+        if (len(self.stackFreeList) > 0):
+            sid = self.stackFreeList.pop()
+        else:
+            sid = self.stackOffset
+            self.stackOffset += 8
+        return sid
+        
+    def memLocDeAlloc(self, memLoc):
+        loc = memLoc.loc
+        if (memLoc.isReg):
+            if (loc < self.paramRegCount):
+                self.paramRegisters.append(loc)
+            else:
+                self.nonParamRegisters.append(loc)
+        else:
+            stackFreeList.append(memLoc.loc)
+                            
+    def paramAlloc(self):
+        # Q & A :)
+        #! don't defend. Stuff to do.
+        if(self.paramCount > 0):
+            if(self.paramRegCount > 0):
+                #paramLabelsOnRegisters = math.min(self.paramCount, self.paramRegCount)
+                #paramLabelsOnStack = 0
+                #diff = self.paramCount - paramLabelsOnRegisters
+                #if (diff > 0):
+                #    paramLabelsOnStack = diff
+                paramLabelsOnRegisters = self.paramCount
+                paramLabelsOnStack = 0
+            else:
+                paramLabelsOnStack = self.paramCount
+                
+            # (for what it is worth) Any paramregs over the paramcount are
+            # available                
+            self.paramRegisters = [reg for reg in range(paramLabelsOnRegisters, self.paramRegCount)]
+
+            for rid in range(0, paramLabelsOnRegisters):
+                print(str(rid))
+                # Q leave parameter on register? 
+                # A if scope within 1 call of param (the start)?
+                prevCallIdx = self.startAccess[self.paramAssocs[rid]].prevCallIdx
+                if (prevCallIdx < 2):
+                    print("reg param value close enough to code to leave param on register; rid {}".format(rid))
+                    # mark any first funcCall for paramReg protection
+                    if (prevCallIdx == 1):
+                        self.callParamRegProtection[0] = [idx] 
+                else:
+                    # Move param value somewhere
+                    # subject of an initial param transfer, the register
+                    # is deallocated.
+                    self.paramRegisterDealloc(rid)
+                    #Q can it be stashed on a nonParam register?
+                    #? and if it is distant/somewhat usused, should it be?
+                    if (self.nonParamRegisterCanAlloc()):
+                        print("reg param value moved to nonParamRegister; rid {}".format(rid))
+                        rid = self.nonParamRegisterAlloc()
+                        # mark paramRegister for transfer to nonParamRegister
+                        trans = (regMemLoc(idx), regMemLoc(rid))
+                        self.initialParamTransfer.append(trans) 
+                    else:
+                        # onto stack
+                        print("reg param value moved to nonParamRegister; rid {}".format(rid))
+                        sid = self.stackAlloc()
+                        # mark paramRegister for transfer to stack
+                        locs = (regMemLoc(idx), stackMemLoc(sid))
+                        self.initialParamTransfer.append(locs) 
+                        
+                        # Q on use, do we need to transfer from stack?
+                        #? do this here, or later?
+                        #if (not param.stayonStack)
+                        # mark stackVal for transfer to register
+                        #rid = ???
+                        #locs = (stackMemLoc(sid), regMemLoc(rid))
+                        #locInitialize[idx] = locs
+            #for idx in range(0, paramLabelsOnStack):
+                # Nothing to do?
+                
+    def localAlloc(self):
+        for ss in self.startStops:
+            # Q Where do non-param locals go?
+            # A if register available and scope less then 1 call, on register
+            #! not dealing with offsets/stack storage
+            if (not ss.isStart):
+                # mem location is now free
+                memLoc = self.localAllocs[ss.idx]
+                self.memLocDeAlloc(memLoc)
+            if (ss.isStart):
+                #! paramscheck
+                paramAssocs
+                if (ss.idx in self.scopeToParam):
+                    # ok, we found a scope which labels a parameter
+                
+                
+                if (self.nonParamRegisterCanAlloc()):
+                    rid = self.nonParamRegisterAlloc()
+                    print("local on nonParamRegister; lid:{} rid {}".format(ss.idx, rid))
+                    self.localAllocs[ss.idx] = regMemLoc(rid)
+                else:
+                    callsInScope = ss.callCount
+                    if (self.paramRegisterCanAlloc() and (callsInScope < 1)):
+                        rid = self.nonParamRegisterAlloc()
+                        print("local on nonParamRegister; lid:{} rid {}".format(ss.idx, rid))
+                        self.localAllocs[ss.idx] = regMemLoc(rid)
+                    else:
+                        raise Error( "No registrs, no solution: data{}".format(ss))
+
+                        # onto stack
+                        #print("reg param value moved to nonParamRegister; rid {}".format(rid))
+                        #sid = self.stackAlloc()
+            
+    def resultStr(self):
+        return "LiveAllocateData(nonParamRegistersToProtect:{}, initialParamTransfer:{}, locInitialize:{}, callParamRegProtection:{})".format(
+            self.nonParamRegistersToProtect,
+            self.initialParamTransfer,
+            self.locInitialize, 
+            self.callParamRegProtection,
+        )
+
+    def stateStr(self):
+        return "LiveAllocateState(paramRegisters:{}, nonParamRegisters:{}, stackOffset:{}, stackFreeList:{})".format(
+            self.paramRegisters,
+            self.nonParamRegisters,
+            self.stackOffset, 
+            self.stackFreeList,
+        )        
+        
+    def __repr__(self):
+        return "LiveAllocate(paramCount:{}, paramAssocs:{}, paramRegCount:{}, labelCount:{})".format(
+            self.paramCount,
+            self.paramAssocs,
+            self.paramRegCount, 
+            self.labelCount,
+            #self.startStops,
+            )       
+    
 def localsAllocate(
     paramRegCount, 
     nonParamRegCount, 
@@ -431,83 +673,115 @@ def testStartStopBuilder():
     # inline scopes
     b = StartStopBuilder()
     b.firstUse(0)
-    b.use(0, 0)
+    b.use(0)
     b.firstUse(1)
-    b.use(1, 0)
+    b.use(1)
     #print(str(b))
     print(b.result())
 
     # callCount handling
     b = StartStopBuilder()
     b.firstUse(0)
-    b.use(0, 1)
+    b.call()
+    b.use(0)
     b.firstUse(1)
-    b.use(1, 3)
+    b.call()
+    b.call()
+    b.use(1)
     #print(str(b))
     print(b.result())
 
+    # callCount dead calls
+    b = StartStopBuilder()
+    b.call()
+    b.firstUse(0)
+    b.use(0)
+    b.call()
+    b.call()
+    #print(str(b))
+    print(b.result())
+    
     # multiple use scope
     b = StartStopBuilder()
     b.firstUse(0)
-    b.use(0, 0)
-    b.use(0, 1)
-    b.use(0, 3)
+    b.call()
+    b.use(0)
+    b.call()
+    b.use(0)
+    b.call()
+    b.use(0)
     #print(str(b))
     print(b.result())
         
     # overlaid scopes
     b = StartStopBuilder()
     b.firstUse(0)
-    b.use(0, 0)
+    b.use(0)
     b.firstUse(1)
-    b.use(1, 0)
-    b.use(0, 0)
+    b.use(1)
+    b.use(0)
     #print(str(b))
     print(b.result())
                
 
-def testlocalsAllocate():
-    # 2 flat allocs
-    testStartStops0 = [
-        startStopStart(0),
-        startStopUse(0),
-        startStopStart(1),
-        startStopUse(1),
+def testliveAllocate():
+    testStartStops = [
+        # newsize
+        startStopStart(0, 0),
+        # sb
+        startStopStart(1, 0),
+        # extralen
+        startStopStart(2, 0),
+        startStopUse(2, 0),
+        startStopUse(0, 0),
+        startStopUse(1, 1)
         ]
-    # 3 overlap allocs
-    testStartStops1 = [
-        startStopStart(0),
-        startStopStart(1),
-        startStopStart(2),
-        startStopUse(1),
-        startStopUse(0),
-        startStopUse(2),
-        ]
-    # param and alloc
-    testStartStops2 = [
-        startStopParamDeclare(0),
-        startStopParamUse(0),
-        startStopStart(1),
-        startStopUse(1),
-        ]
-    # param and alloc overlap
-    testStartStops3 = [
-        startStopParamDeclare(0),
-        startStopStart(1),
-        startStopUse(1),
-        startStopParamUse(0),
-        ]
-    la = localsAllocate(
-        NonParamRegisters,
-        ParamRegisters, 
-        testStartStops3, 
-        # offloadParams
-        False
-        #True
+        
+    ll = LiveAllocate(
+        paramCount = 2,
+        paramAssocs = {0:1, 1:2}, 
+        paramRegCount = 6, 
+        nonParamRegCount = 5, 
+        startStops = testStartStops,
+        labelCount = 3,
         )
-    print(la)
+    ll.paramAlloc()
+    ll.localAlloc()
+    print(ll)
+    print(ll.stateStr())
+    print(ll.resultStr())
     
- 
+    # # 2 flat allocs
+    # testStartStops0 = [
+        # startStopStart(0),
+        # startStopUse(0),
+        # startStopStart(1),
+        # startStopUse(1),
+        # ]
+    # # 3 overlap allocs
+    # testStartStops1 = [
+        # startStopStart(0),
+        # startStopStart(1),
+        # startStopStart(2),
+        # startStopUse(1),
+        # startStopUse(0),
+        # startStopUse(2),
+        # ]
+    # # param and alloc
+    # testStartStops2 = [
+        # startStopParamDeclare(0),
+        # startStopParamUse(0),
+        # startStopStart(1),
+        # startStopUse(1),
+        # ]
+    # # param and alloc overlap
+    # testStartStops3 = [
+        # startStopParamDeclare(0),
+        # startStopStart(1),
+        # startStopUse(1),
+        # startStopParamUse(0),
+        # ]
+        
 def testActionCode():
     b = []
     localAlloc = [
@@ -521,8 +795,8 @@ def testActionCode():
     
 def main():
     #testMangle()
-    #testlocalsAllocate()
-    testStartStopBuilder()
+    #testStartStopBuilder()
+    testliveAllocate()
     #r = testActionCode()
     #print("\n".join(r))
     
